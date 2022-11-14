89726684832960cf5c21972b2693773f3af03d00
wuchunran <wuchunran@authing.cn>
2022-11-14 21:28:11 +0800

    增加devTest

---
+++ src/main/java/com/authing/kafka/producer/DevTesst.java
@@ -0,0 +1,12 @@
+package com.authing.kafka.producer;
+
+/**
+ * @author rebel
+ * @date 2022/11/14 21:27
+ */
+public class DevTesst {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
